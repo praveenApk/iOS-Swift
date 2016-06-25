dfdc0623c42d29046c90f4fb42abe963c8e28951
Praveen <praveen.a@caratlane.com>
2016-06-25 12:29:47 +0530

    Initial Commit

---
+++ HitListCoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HitListCoreData
+//
+//  Created by PraveenApk on 25/06/16.
+//  Copyright © 2016 PraveenApk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
